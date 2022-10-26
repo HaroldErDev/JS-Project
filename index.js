d66dd571e275760e419d3d5fa174f0872f9d6d57
Tiziano <tinaritiziano.dev@gmail.com>
2022-10-26 22:48:28 +0200

    Added scores and title

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cells = document.querySelectorAll('.cell');
 const restart = document.querySelector('button');
+const player1Score = document.querySelector('.player1');
+const player2Score = document.querySelector('.player2');
 
 const winCombinations = [
     [0,1,2],
@@ -14,6 +16,11 @@ const winCombinations = [
 
 let player1 = [];
 let player2 = [];
+
+let score = {
+    player1: 0,
+    player2: 0
+}
 
 let turn = true;
 
@@ -38,19 +45,37 @@ function checkWinner() {
     winCombinations.find((item) => {
         if(item.filter((i) => player1.includes(i)).length === 3) {
             alert('Player1 Vince!');
+            score.player1++;
+            drewScore();
+            cleanField();
             return item;
         } else if(item.filter((i) => player2.includes(i)).length === 3) {
             alert('Player2 Vince!');
+            score.player2++;
+            drewScore();
+            cleanField();
             return item;
         }
         return;
     });
 }
 
-restart.addEventListener('click', () => {
+function drewScore() {
+    player1Score.innerHTML = "Player1: " + score.player1;
+    player2Score.innerHTML = "Player2: " + score.player2;
+}
+
+drewScore();
+
+function cleanField() {
     for(let i=0; i<cells.length; i++) {
         cells[i].innerHTML = '';
     }
     player1.length = 0;
     player2.length = 0;
+    turn = true;
+}
+
+restart.addEventListener('click', () => {
+    cleanField();
 });
